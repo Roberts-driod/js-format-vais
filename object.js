f4ca9a05b76dbc2420247daad140185fc9b71cbc
Emils <emils@gmail.com>
2025-10-21 13:18:20 +0300

    auto object

---
+++ object.js
@@ -0,0 +1,14 @@
+
+let auto = {
+    marka: "audi",
+    model: "A6",
+    year: 2006,
+};
+
+    console.log(auto["marka"] + " and "  + auto.marka);
+
+   let currentYear = 2025;
+   if( currentYear - auto.year  >= 10) {
+        console.log("Auto ir vecāks par 10 gadiem")
+   } else {console.log('Auto nav vecāks par 10 gadiem'); } 
+ 
