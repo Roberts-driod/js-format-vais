2ebf166dca1d35a8dd2d17e8a0fb2830d5ae8b9e
Emils <emils@gmail.com>
2025-10-21 13:10:40 +0300

    inSequence function

---
+++ function.js
@@ -0,0 +1,14 @@
+
+
+    function intSequence (first, last){
+        while(first <= last){
+            console.log(first);
+           first += 1;
+        } 
+    }
+
+        intSequence(5,8);
+        console.log("\n");
+        intSequence(1,10);
+        console.log("\n");
+        intSequence(7,16);
